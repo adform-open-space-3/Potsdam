87bd06b2e1df87283b1ea4e94083f35fcd895c39
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2012-09-11 22:33:15 +0300

    Remove config.js, move everything to app.js

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ var express = require('express');
 var app = module.exports = express();
 var mongoose = require('mongoose');
 
-var config = require('./config.js')(app, express, mongoose);
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'topsecret' }));
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(express.logger());
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
 
 require('./routes')(app);
 
