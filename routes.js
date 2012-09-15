85f6db92eebfa5e5c93591e7db835d0bdff590a8
Erikas <erikas.balynas@gmail.com>
2012-09-15 16:29:04 +0300

    index view lists all presentations

--- routes.js
+++ routes.js
@@ -1,9 +1,7 @@
 module.exports = function(app, models){
   app.get('/', function(req, res){
-    models.feedbacks.find({}, function(err, docs){
-      res.render('index', {
-          feedbacks: docs
-      });
+    res.render('index', {
+        agenda: models.agenda
     });
   });
 
