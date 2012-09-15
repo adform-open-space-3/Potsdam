3b27b42559352a32beeb323913578228b5dbaffd
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2012-09-15 18:12:57 +0300

    Remove agenda route and view

--- routes.js
+++ routes.js
@@ -5,12 +5,6 @@ module.exports = function(app, models){
     });
   });
 
-  app.get('/agenda', function(req, res){
-    res.render('agenda', {
-      agenda: models.agenda
-    });
-  });
-
   app.get('/:presenter', function(req, res){
     var presentation;
     for (var i in models.agenda) {
