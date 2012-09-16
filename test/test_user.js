a82575c5da940e4ae0dba64246c14bc1d2b068d1
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2012-09-16 09:24:02 +0300

    Now that we see what happens when test fails, fix it!

--- test/test_user.js
+++ test/test_user.js
@@ -5,7 +5,9 @@ describe('User', function() {
   describe('#getFeedback()', function() {
     it('should return an empty object when there are no feedbacks', function() {
       var user = new models.User();
-      user.getFeedback().should.equal({});
+      user.getFeedback().should.not.have.property('presenter');
+      user.getFeedback().should.not.have.property('rating');
+      user.getFeedback().should.not.have.property('comment');
     });
   });
 });
