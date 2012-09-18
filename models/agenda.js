e2e8931a80201cb6a38d14a637a1c1f2c99f120a
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2012-09-18 21:52:48 +0300

    Use new agenda.json. Show random img if there're more than one presenter.

--- models/agenda.js
+++ models/agenda.js
@@ -8,6 +8,19 @@ var urlify = require('urlify').create( {
 var agenda = require('./agenda.json');
 
 agenda.forEach(function(element) {
+  var presenters = element.Presenters;
+
+  element.Presenter = presenters.map(function(presenter) {
+    return presenter.Name;
+  }).join(', ');
+
+  element.Img = function() {
+    if (presenters.length === 1) {
+      return presenters[0].Img;
+    }
+    return presenters[Math.floor(Math.random()*presenters.length)].Img;
+  };
+
   element.Url = urlify(element.Presenter).toLowerCase();
 });
 
